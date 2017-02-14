918b06adcb587e8cb1c3e64b8bd3f746c4d77627
Phat Tran <phatdtran3k6@gmail.com>
2017-02-14 02:13:35 -0500

    Beta Release: 	+ TODO: seekTo method doesn't work 	+ TODO: old videoNail shown instead of current

--- content.js
+++ content.js
@@ -1,36 +1,96 @@
-var meta_list = document.getElementById("watch7-content").getElementsByTagName("meta");
+if (!document.getElementById("iframe-setup")) {
+    var tag = document.createElement('script');
+    tag.async = false;
+    tag.id = "iframe-setup";
+    tag.src = "https://www.youtube.com/iframe_api";
+    var firstScriptTag = document.getElementsByTagName("script")[0];
+    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+    console.log("Youtube API setup");
+}
+
+
+if (document.getElementById("script-container")) {
+    element = document.getElementById("script-container");
+    document.getElementById("script-container").parentNode.removeChild(element);
+}
+var container = document.createElement("div");
+container.id = "script-container";
+document.body.appendChild(container);
+
+var iframe_script = document.createElement('script');
+iframe_script.type = "text/javascript";
+iframe_script.innerHTML = 
+`
+// Append placeholder for player
+var video_div = document.createElement('div');
+video_div.id = "videoNail"
+document.getElementsByClassName("yt-masthead-logo-container").item(0).appendChild(video_div);
+
+var scrollDistance = 450;
+var playerIsVisible = false;
 var video_id = "";
+var curr_time = 0;
 
-for (var i = 0; i < meta_list.length; i++) { 
-    if (meta_list[i].getAttribute("itemprop") == "videoId") { 
-        video_id = meta_list[i].getAttribute("content");
-        break; 
+video_id = window.location.search.substring(3);
+console.log(video_id);
+
+if (typeof loaded == undefined) {
+    var loaded = false;
+}
+
+// Set up player definition
+// Hooking up to placeholder above
+var main_player = document.getElementById('movie_player');
+var player; 
+function onYouTubeIframeAPIReady() {  
+    player = new YT.Player('videoNail', { 
+        height: '0', 
+        width: '0', 
+        videoId: video_id
+    }); 
+    loaded = true;
+    console.log("My player with this id: " + video_id);
+}
+
+if (loaded) {
+    onYouTubeIframeAPIReady();
+}
+
+window.onscroll = function(e) {
+    if (!loaded) {
+        return;
     }
-} 
-
-var node = document.createElement("embed");
-node.src = "https://www.youtube.com/v/" + video_id + "?autoplay=1&t=2000";
-node.width = "320";
-node.height = "240";
-
-var threshold = 300;
-var video_appended = false;
-
-$(window).on('scroll', function(){
     // When user scrolls down
-    if($(window).scrollTop() > threshold){
-        document.getElementsByClassName("yt-masthead-logo-container").
-        item(0).appendChild(node);
-        video_appended = true; 
+    if(window.pageYOffset > scrollDistance && !playerIsVisible){
+        // TODO: don't start videoNail when video already ended
+        curr_time = main_player.getCurrentTime();
+        player.setSize(320, 180);
+        //player.seekTo(curr_time, true);
+        player.setVolume(main_player.getVolume());
+        if (main_player.getPlayerState() == YT.PlayerState.PAUSED) {
+            player.pauseVideo();
+        } else {
+            player.playVideo();
+        }
+        main_player.mute();
+        player.unMute();
+        playerIsVisible = true;
     }
 
-    // When user scrolls up, the condition below 
-    // ensures that there exists a video node when we call removeChild
-    if (($(window).scrollTop() < threshold) && video_appended) {
-        document.getElementsByClassName("yt-masthead-logo-container").
-        item(0).removeChild(node);
-        video_appended = false;
+    // When user scrolls up
+    if (window.pageYOffset < scrollDistance && playerIsVisible) {
+        curr_time = player.getCurrentTime();
+        player.setSize(0, 0);
+        //main_player.seekTo(curr_time, true);
+        main_player.setVolume(player.getVolume());
+        if (player.getPlayerState() == YT.PlayerState.PAUSED) {
+            main_player.pauseVideo();
+        } else {
+            main_player.playVideo();
+        }
+        player.mute();
+        main_player.unMute();
+        playerIsVisible = false;
     }
-});
-
-
+};`
+document.getElementById("script-container").appendChild(iframe_script);
