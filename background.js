29f4e4191c1f63e816ad2015a2b36bd086b31c02
Phat Tran <phatdtran3k6@gmail.com>
2017-02-22 14:13:13 -0500

    Release 0.0.8: 	+ Improved algorithm to detect current tab's URL to prevent 	video-nail popping up in other YouTube's pages (feeds, 	subscriptions, trending, etc.) 	+ Removed "removeVideoNail.js"

--- background.js
+++ background.js
@@ -3,12 +3,17 @@ chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
     // on other pages (trending page, channel page, home page, etc.)
     if (changeInfo.url) {
         if (changeInfo.url.includes("youtube.com/watch?v=")) {
-            chrome.tabs.executeScript({file: "content.js"});
-        } else if (changeInfo.url.includes("youtube.com")) {
-            chrome.tabs.executeScript({file: "removeVideoNail.js"});
+            chrome.tabs.query({active: true, lastFocusedWindow: true}, function(tabs) {
+                var tab = tabs[0];
+                if (tab.url.includes("youtube.com/watch?v=")) {
+                    chrome.tabs.executeScript({file: "content.js"});
+                    chrome.tabs.insertCSS({file: "videonail.css"});
+                }
+            });
         }
     }
 });
+
 
 chrome.browserAction.onClicked.addListener(function(tab) {
     chrome.tabs.executeScript({file: "toggle.js"});
--- removeVideoNail.js
+++
@@ -1,5 +0,0 @@
-// If "video-nail" div does exist, remove it
-if (document.getElementById("video-nail")) {
-    var videoPlaceholder = document.getElementById("video-nail");
-    videoPlaceholder.parentNode.removeChild(videoPlaceholder);
-}
