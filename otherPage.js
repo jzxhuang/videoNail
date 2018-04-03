0de4c5eb6c2912e0eab7cda5a1a20f9f9f0fc2d0
Phat Tran <phat.tran@genesys.com>
2018-04-03 19:21:30 -0400

    Updated otherPage.js to show video on other YouTube pages; Refactored code

--- otherPage.js
+++ otherPage.js
@@ -1,20 +1,70 @@
-if (document.getElementById("video-nail")) {
-    // Remove script-container (which includes iframe.js)
-    if (document.getElementById("script-container")) {
-        var scriptContainer = document.getElementById("script-container");
-        scriptContainer.parentNode.removeChild(scriptContainer);
-        console.log("Remove script-container.");
+function addScriptContainer() {
+  return new Promise((resolve, reject) => {
+    if (document.querySelector("#script-container")) {
+      let scriptContainer = document.querySelector("#script-container");
+      scriptContainer.parentNode.removeChild(scriptContainer);
     }
 
-    if (document.getElementById("iframe-setup")) {
-        var iframeSetup = document.getElementById("iframe-setup");
-        iframeSetup.parentNode.removeChild(iframeSetup);
-    }
-
-    var videoPlaceholder = document.getElementById("video-nail");
-    videoPlaceholder.parentNode.removeChild(videoPlaceholder);
-
-    window.onscroll = (e) => {
-        return;
-    }
+    var scriptContainer = document.createElement("div");
+    scriptContainer.id = "script-container";
+    document.body.appendChild(scriptContainer);
+    resolve();
+  })
 }
+
+function setVideoEventHandlers() {
+  return new Promise((resolve, reject) => {
+    if (document.getElementById("vidEventHandlers")) {
+      let oldHandlerScript = document.getElementById("vidEventHandlers");
+      oldHandlerScript.parentNode.removeChild(vidHandlerScript);
+    }
+    var vidHandlerScript = document.createElement('script');
+    vidHandlerScript.type = "text/javascript";
+    vidHandlerScript.id = "vidEventHandlers";
+    vidHandlerScript.src = chrome.extension.getURL("vidEventHandlers.js");
+    document.getElementById("script-container").appendChild(vidHandlerScript);
+    vidHandlerScript.onload = resolve;
+  })
+}
+
+function addIframeAPI() {
+  return new Promise((resolve, reject) => {
+    if (document.querySelector("#iframe-API")) {
+      let iframeAPI = document.querySelector("#iframe-API");
+      iframeAPI.parentNode.removeChild(iframeAPI);
+    }
+    var iframeAPI = document.createElement('script');
+    iframeAPI.id = "iframe-API";
+    iframeAPI.src = "https://www.youtube.com/iframe_api";
+    var firstScriptTag = document.getElementsByTagName("script")[0];
+    firstScriptTag.parentNode.insertBefore(iframeAPI, firstScriptTag);
+  }) 
+}
+
+function addVideonailContainer() {
+  if (document.getElementById("videonail-container")) {
+    let oldContainer = document.getElementById("videonail-container");
+    oldContainer.parentNode.removeChild(oldContainer);
+  }
+
+  // Create #videonail div and insert it inside #videonail-container div
+  var videoNailContainer = document.createElement('div');
+  videoNailContainer.id = "videonail-container";
+  var videoNail = document.createElement('div');
+  videoNail.id = "videonail";
+  videoNailContainer.appendChild(videoNail);
+  document.getElementById("content").appendChild(videoNailContainer);
+}
+
+addScriptContainer()
+  .then(_ => {
+    return setVideoEventHandlers();
+  })
+  .then(_ => {
+    return addIframeAPI();
+  })
+  .catch(err => {
+    console.log(err);
+  });
+
+addVideonailContainer();
---
+++ vidEventHandlers.js
@@ -0,0 +1,27 @@
+
+// TODO: Fetch time, speed, width, height, id from storage
+var time = 0;
+var speed = 0;
+var id = "AHX6tHdQGiQ";
+var player;
+
+function onYouTubeIframeAPIReady() {
+  player = new YT.Player('videonail', {
+    // TODO: Set width, height of the player
+    height: "502",
+    width: "282",
+    videoId: id,
+    events: {
+      'onReady': onPlayerReady
+    }
+  });
+  // player.setSize(width, height);
+  // player.seekTo(time, true);
+  // player.setPlaybackRate(speed);
+  // player.playVideo();
+}
+
+function onPlayerReady(event) {
+  // TODO: Set all metadata (curr time, speed) to the video (event.target)
+  event.target.playVideo();
+}
