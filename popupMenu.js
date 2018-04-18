4cc7b312b29fb1b23f0e5f5f0628b7df871f8530
Max <ptakh.maksym@gmail.com>
2018-04-17 21:46:48 -0400

    stores Video Nail state in chrome storage

--- popupMenu.js
+++ popupMenu.js
@@ -1,10 +1,14 @@
 var VN_enabled = true;
 
 window.onload = function() {
-    chrome.management.getSelf(function(extensionInfo) {
-        VN_enabled = extensionInfo.enabled;
+    // chrome.management.getSelf(function(extensionInfo) {
+    //     VN_enabled = extensionInfo.enabled;
+    // });
+    chrome.storage.local.get('VN_state', function(state) {
+        VN_enabled = state.VN_state.enabled;
+        console.log(state.VN_state.enabled);
+        document.getElementById('VN_switch').checked = VN_enabled;
     });
-    document.getElementById('VN_switch').checked = VN_enabled;
 
     document.getElementById('VN_switch').addEventListener('click', function() {
         toggleVN();
@@ -13,7 +17,13 @@ window.onload = function() {
 
 function toggleVN() {
     VN_enabled = !VN_enabled;
-    chrome.management.getSelf(function(extensionInfo) {
-        chrome.management.setEnabled(extensionInfo.id, VN_enabled);
-    });
+    var state = {
+        VN_state: {
+            enabled: VN_enabled
+        }
+    }
+    chrome.storage.local.set(state);
+    // chrome.management.getSelf(function(extensionInfo) {
+    //     chrome.management.setEnabled(extensionInfo.id, VN_enabled);
+    // });
 }
